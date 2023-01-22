93c92f8be7c31c5ecfea9d1ff72b0971497a435a
My-selforever <72210243+My-selforever@users.noreply.github.com>
2023-01-22 15:09:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { View } from 'react-native';
+import Card from './Components/Card';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Card />
+      </View>
+    );
+  }
+}
